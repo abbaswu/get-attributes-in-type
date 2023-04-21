83d4395d557df00e5791bf7a6a278d3da46ddfcc
Jifeng Wu <jifengwu2k@gmail.com>
2023-04-20 19:33:33 -0700

    2023-04-21T 2:33:33+0000

--- get_dynamic_attributes_in_type.py
+++ get_dynamic_attributes_in_type.py
@@ -2,7 +2,7 @@ from dis import Bytecode
 from enum import Enum, auto
 from functools import lru_cache
 from typing import Generator
-from types import CodeType, FunctionType
+from types import CodeType, FunctionType, MethodType
 
 
 class State(Enum):
@@ -10,14 +10,17 @@ class State(Enum):
     SELF_LOADED = auto()
 
 
-# looks for the following two bytecode sequences
+# looks for the following three bytecode sequences
 # 10 LOAD_FAST                0 (self)
 # 12 LOAD_ATTR                2 (update)
 # and
 # 66 LOAD_FAST                0 (self)
 # 68 STORE_ATTR               5 (_io_refs)
+# and
+# 0 LOAD_FAST                0 (self)
+# 2 LOAD_METHOD              0 (parse)
 # uses the state machine design pattern
-def get_attributes_accessed_on_self_in_code(code: CodeType) -> Generator[str, None, None]:
+def get_attributes_accessed_on_self_in_method_code(code: CodeType) -> Generator[str, None, None]:
     state = State.SELF_NOT_LOADED
 
     for instruction in Bytecode(code):
@@ -30,42 +33,41 @@ def get_attributes_accessed_on_self_in_code(code: CodeType) -> Generator[str, No
         elif state == State.SELF_LOADED:
             # Instruction(opname='LOAD_ATTR', opcode=106, arg=2, argval='update', argrepr='update', offset=12, starts_line=None, is_jump_target=False)
             # Instruction(opname='STORE_ATTR', opcode=95, arg=5, argval='_io_refs', argrepr='_io_refs', offset=68, starts_line=None, is_jump_target=False)
-            if instruction.opname in ('LOAD_ATTR', 'STORE_ATTR'):
+            # Instruction(opname='LOAD_METHOD', opcode=160, arg=0, argval='parse', argrepr='parse', offset=2, starts_line=None, is_jump_target=False)
+            if instruction.opname in ('LOAD_ATTR', 'STORE_ATTR', 'LOAD_METHOD'):
                 yield instruction.argval
 
             state = State.SELF_NOT_LOADED
 
 
-def get_dynamic_attributes_accessed_on_self_in_constructor(type: type) -> set[str]:
-    # find `__init__`
-    if '__init__' in type.__dict__:
-        __init__ = type.__dict__['__init__']
-    else:
-        __init__ = None
+def get_dynamic_attributes_accessed_on_self_in_methods(type: type) -> set[str]:
+    dynamic_attributes_in_type: set[str] = set()
 
-    # only support constructors of `FunctionType` (i.e., user-defined functions)
-    if isinstance(__init__, FunctionType):
-        dynamic_attributes_in_type: set[str] = set(
-            get_attributes_accessed_on_self_in_code(
-                __init__.__code__
+    for key, value in type.__dict__.items():
+        # only support methods of `FunctionType` (i.e., user-defined non-`classmethod`, non-`staticmethod` methods)
+        # https://medium.com/solomons-tech-stack/python3-how-to-inspect-a-functions-type-9999eeb31c0b
+        if isinstance(value, FunctionType):
+            dynamic_attributes_in_type.update(
+                get_attributes_accessed_on_self_in_method_code(
+                    value.__code__
+                )
             )
-        )
+        else:
+            pass
 
-        return dynamic_attributes_in_type
-    else:
-        return set()
+    return dynamic_attributes_in_type
 
 
 @lru_cache(maxsize=None)
 def get_dynamic_attributes_in_type(type: type) -> set[str]:
     if not type.__bases__:
-        return get_dynamic_attributes_accessed_on_self_in_constructor(type)
+        return get_dynamic_attributes_accessed_on_self_in_methods(type)
     else:
         dynamic_attributes_in_type: set[str] = set()
 
         for base in type.__bases__:
             dynamic_attributes_in_type.update(get_dynamic_attributes_in_type(base))
 
-        dynamic_attributes_in_type.update(get_dynamic_attributes_accessed_on_self_in_constructor(type))
+        dynamic_attributes_in_type.update(get_dynamic_attributes_accessed_on_self_in_methods(type))
 
         return dynamic_attributes_in_type
